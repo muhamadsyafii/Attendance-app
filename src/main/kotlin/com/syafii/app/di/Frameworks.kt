6601e61a29ac845343327fa399069d29ad06b113
Muhamad Syafii <muhamadsyafii4@gmail.com>
2025-07-17 09:23:47 +0700

    Feat : - add register account

--- src/main/kotlin/com/syafii/app/di/Frameworks.kt
+++ src/main/kotlin/com/syafii/app/di/Frameworks.kt
@@ -1,14 +1,13 @@
 package com.syafii.app.di
 
-import com.syafii.core.domain.model.JwtConfig
 import com.syafii.core.domain.repository.GreetingRepository
 import com.syafii.core.domain.repository.UserRepository
 import com.syafii.core.domain.usecase.GetGreetingUseCase
 import com.syafii.core.domain.usecase.GetGreetingUseCaseImpl
 import com.syafii.core.domain.usecase.login.LoginUseCase
+import com.syafii.core.domain.usecase.register.RegisterUseCase
 import com.syafii.data.repository.GreetingRepositoryImpl
 import com.syafii.data.repository.UserRepositoryImpl
-import io.ktor.server.application.Application
 import org.koin.dsl.module
 
 
@@ -17,15 +16,5 @@ val koinModule = module {
     single<GetGreetingUseCase> { GetGreetingUseCaseImpl(get()) }
     single<UserRepository> { UserRepositoryImpl() }
     single { LoginUseCase(get(), get()) }
-
-/*    single {
-        val env = get<Application>().environment
-        val jwtConfig = env.config.config("jwt")
-        JwtConfig(
-            secret = jwtConfig.property("secret").getString(),
-            issuer = jwtConfig.property("issuer").getString(),
-            audience = jwtConfig.property("audience").getString()
-        )
-    }*/
-
+    single { RegisterUseCase(get()) }
 }
